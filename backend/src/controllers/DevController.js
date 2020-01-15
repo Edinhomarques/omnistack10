576f5bfaf6304a377d73c38950d38f948d1d3107
Edson Marques <Edson.AlvesFilho@cencosud.com.br>
2020-01-15 10:42:52 -0300

    Criação das novas rotas,criação da função parseStringAsArray

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,31 +1,39 @@
 const Dev = require('../models/Dev');
-const axios = require('axios')
+const axios = require('axios');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 module.exports = {
     async storeDev(request, response){
         const { github_username, techs, latitude, longitude } = request.body;
-    
-        const techsArray = techs.split(',').map(tech => tech.trim());
         let dev = await Dev.findOne({github_username}) 
-
-        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
         
-        const  { name = login, avatar_url, bio } = apiResponse.data    
-        
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude]
-        }
         if(!dev){
-            dev = await Dev.create({
-                github_username,
-                name,
-                avatar_url,
-                bio,
-                techs: techsArray,
-                location
-            });
+            const techsArray = parseStringAsArray(techs)
+            
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+            
+            const  { name = login, avatar_url, bio } = apiResponse.data    
+            
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+
+                dev = await Dev.create({
+                    github_username,
+                    name,
+                    avatar_url,
+                    bio,
+                    techs: techsArray,
+                    location
+                });
         }   
         return response.json(dev)
     },
+
+    async listDevs(request, response){
+        const devs = await Dev.find();
+
+        return response.json(devs)
+    }
 
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const DevController = require('./controllers/DevController')
+const SearchControoler = require('./controllers/SearchController')
 const routes = Router();
 
+routes.get('/devs', DevController.listDevs)
 routes.post('/devs', DevController.storeDev)
+routes.get('/search', SearchControoler.searchDev)
 
 module.exports = routes
